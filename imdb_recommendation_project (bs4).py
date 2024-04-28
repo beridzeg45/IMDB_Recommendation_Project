5d3dd5e7bf420b567bfcb7f50d91ab8dc4756731
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-04-28 18:48:30 +0400

    Add files via upload

--- imdb_recommendation_project (bs4).py
+++ imdb_recommendation_project (bs4).py
@@ -17,7 +17,6 @@ path=r'D:\python data\movie_recommendation_project'
 
 
 
-
 all_movie_urls=pickle.load(open(os.path.join(path,'all_movie_urls.pickle'),'rb'))
 len(all_movie_urls)
 
@@ -32,7 +31,7 @@ def return_movie_info(movie_url):
         pass
     
     try:
-        poster=soup.select_one('div[class="ipc-poster ipc-poster--baseAlt ipc-poster--dynamic-width ipc-sub-grid-item ipc-sub-grid-item--span-2"] img').get_attribute('src')
+        poster=soup.select_one('div[class="ipc-poster ipc-poster--baseAlt ipc-poster--dynamic-width ipc-sub-grid-item ipc-sub-grid-item--span-2"] img')['src']
     except:
         poster=None
 
@@ -42,12 +41,12 @@ def return_movie_info(movie_url):
         movie_title=None
 
     try:
-        year=soup.select_one('ul[class="ipc-inline-list ipc-inline-list--show-dividers sc-d8941411-2 cdJsTz baseAlt"]').text
+        year=soup.select_one('ul[class="ipc-inline-list ipc-inline-list--show-dividers sc-d8941411-2 cdJsTz baseAlt"]').get_text(separator='|')
     except:
         year=None
 
     try:
-        rating=soup.select_one('div[class="sc-bde20123-0 dLwiNw"]').text
+        rating=soup.select_one('div[class="sc-bde20123-0 dLwiNw"]').get_text(separator='|')
     except:
         rating=None
 
@@ -62,12 +61,12 @@ def return_movie_info(movie_url):
         plot=None
 
     try:
-        director=soup.select_one('div[class="sc-b7c53eda-3 vXcqY"]').text
+        director=soup.select_one('div[class="sc-b7c53eda-3 vXcqY"]').get_text(separator='|')
     except:
         director=None
 
     try:
-        genres=soup.select_one('div[data-testid="genres"]').text
+        genres=soup.select_one('div[data-testid="genres"]').get_text(separator='|')
     except:
         genres=None
     
@@ -75,9 +74,18 @@ def return_movie_info(movie_url):
     return info_dict
 
 
+max_index=0 
+for file in os.listdir(path): 
+    if 'all_data_' in file: 
+        index=int(file.split('all_data_')[-1].split('.pickle')[0]) 
+        if index>max_index: 
+            max_year=index
+
 all_data=[]
 
 for i,movie_url in enumerate(all_movie_urls,start=1):
+    if i<=max_index:
+        continue
     info_dict=return_movie_info(movie_url)
     all_data.append(info_dict)
 

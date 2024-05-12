4d768fa595bd5274afb9abf181c0bb2b6b11cfe2
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-05-12 16:58:47 +0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,9 @@
 import streamlit as st
 import pandas as pd
-import difflib
 
-# Function to find similar director using difflib
-def find_similar_director(search_director, all_directors):
-    return difflib.get_close_matches(search_director, all_directors, n=1, cutoff=0.6)
+df = pd.read_csv('movies.csv')
 
-# Function to find similar genre using difflib
-def find_similar_genre(search_genre, all_genres):
-    return difflib.get_close_matches(search_genre, all_genres, n=1, cutoff=0.6)
-
-# Function to return similar movies based on selected movie
+#define function
 def return_similar_movies(selected_value):
     selected_value = selected_value.lower()
     filtered_df = df[df['Title'].str.lower().fillna('').str.contains(selected_value)].sort_values('Rating Count', ascending=False)
@@ -22,12 +15,9 @@ def return_similar_movies(selected_value):
     director = filtered_movie['Directors']
     genre = filtered_movie['Genres']
     
-    similar_director = find_similar_director(director, df['Directors'].tolist())
-    similar_genre = find_similar_genre(genre, df['Genres'].tolist())
-
     similar_movies_df = df[
-        (df['Directors'].str.contains(similar_director[0], na=False, case=False)) &
-        (df['Genres'].str.contains(similar_genre[0], na=False, case=False)) &
+        (df['Directors'].str.contains(director, na=False,case=False)) &
+        (df['Genres'].str.contains(genre, na=False,case=False)) &
         (df['Title'].notna())
     ].sort_values(['Rating Count', 'IMDB Rating'], ascending=[False, False])
     
@@ -37,23 +27,24 @@ def return_similar_movies(selected_value):
     
     return similar_movies_titles, similar_movies_posters, similar_movies_urls
 
-# Read data
-df = pd.read_csv('movies.csv')
 
-# Streamlit app
+
+
+#st app
 st.set_page_config(layout="wide")
 
 st.header('Movie Recommendation Project')
-search_value = st.text_input('Type movie title to get recommendations', '')
+input_value = st.text_input('Type movie title to get recommendations', '')
 
-if st.button('Show Recommended Movies') and search_value:
-    titles, posters, urls = return_similar_movies(search_value)
+if st.button('Show Recommended Movies') and input_value:
+    titles, posters, urls = return_similar_movies(input_value)
     cols = st.columns(len(titles))
 
     for i, (title, poster, url) in enumerate(zip(titles, posters, urls)):
         with cols[i]:
             st.markdown(f"<span style='font-size: 14px;'><a href='{url}'>{title}</a></span>", unsafe_allow_html=True)
             st.image(poster)
+
 
 # Add intro text to upper left corner
 intro_text="""
